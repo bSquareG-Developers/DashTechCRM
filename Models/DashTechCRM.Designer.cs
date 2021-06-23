afdcf45247ce6cecb3fd746c884dbd628fc7e0c5
kautilya vaidya <kautilya.dashtechinc@gmail.com>
2021-06-24 02:08:16 +0530

    change connection live server to test server

--- Models/DashTechCRM.Designer.cs
+++ Models/DashTechCRM.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\git\New March Updates 2021\New March Updates 2021\DashTechCRM\Models\DashTechCRM.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\git\DashTechCRM\Models\DashTechCRM.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
