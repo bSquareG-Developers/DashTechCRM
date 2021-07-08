d56f05ee7778de5c8ad4647df4fbb2148c00fdd6
kautilya vaidya <kautilya.dashtechinc@gmail.com>
2021-07-08 19:27:06 +0530

    added OnBoarding area

---
+++ Areas/OnBoarding/Controllers/dashboardController.cs
@@ -0,0 +1,32 @@
+﻿using DashTechCRM.Models;
+using System;
+using System.Data;
+using System.Web.Mvc;
+
+namespace DashTechCRM.Areas.OnBoarding.Controllers
+{
+    public class dashboardController : Controller
+    {
+        // GET: OnBoarding/dashboard
+        DataTable dt = new DataTable();
+        ConnectionDB dl = new ConnectionDB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetOnBoardingDashBoard()
+        {
+            try
+            {
+                dt = dl.GetDataTable("GetOnBoardingDashBoard");
+                return CommonHelperClass._serializeDatatable(dt);
+            }
+            catch (Exception e)
+            {
+                CommonHelperClass.InsertErrorLog(e.Message, "OnBoarding/dashboard/GetOnBoardingDashBoard");
+                throw;
+            }
+        }
+    }
+}
